7647b65a38e6c544ae0f963e1893e2f8dd82b86e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 12:22:01 -0500

    refactor ask_question by adding handle_incorrect method

--- flashcards_controller.rb
+++ flashcards_controller.rb
@@ -1,26 +1,26 @@
 require_relative 'flashcards_model'
 
-# class FlashcardModel
+class FlashcardModel
 
-#   def initialize
-#   end
+  def initialize
+  end
 
-#   def correct?(question, guess)
-#     guess == "2"
-#   end
+  def correct?(question, guess)
+    guess == "2"
+  end
 
-#   def next_question
-#     "What's 1+1?"
-#   end
+  def next_question
+    "What's 1+1?"
+  end
 
-#   def get_answer(question)
-#     "2"
-#   end
+  def get_answer(question)
+    "2"
+  end
 
-#   def shuffle
-#   end
+  def shuffle!
+  end
 
-# end
+end
 
 
 
@@ -31,30 +31,36 @@ class FlashcardController
     start
   end
 
+  def start
+    puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
+    main_menu
+  end
+
   def main_menu
     loop do
       puts "What would you like to do?"
-      puts "1-Begin Game, 2-Shuffle, 3-Quit"
-      input = gets.chomp.to_i
+      puts "1-Begin Game, 2-Shuffle, 3-Exit"
+      input = gets.chomp
       case input
-      when 1
+      when '1'
         begin_game
-      when 2
+      when '2'
         @deck.shuffle!
-      when 3
+      when '3'
+        exit
+      when 'exit'
         exit
       end
     end
   end
 
   def begin_game
-    puts "Press enter blank answer to exit game."
-    while ask_question(@deck.next_question)
-    end    
+    puts "Answer 'exit' to exit game."
+    while ask_question(@deck.next_question); end
   end
 
   def ask_question(question)
-    incorrect_counter = 0
+    tries = 0
 
     loop do 
       puts question
@@ -62,26 +68,29 @@ class FlashcardController
       if @deck.correct?(question, guess)
         puts "Correct!"
         return true
-      elsif guess == ""
+      elsif guess.downcase == "exit"
         return false
       else
-        incorrect_counter += 1
-        if incorrect_counter > 2
-          puts "Incorrect. View answer? y/n"
-          input = gets.chomp
-          next if input == "n"
-          puts @deck.get_answer(question)
-        else
-          puts "Incorrect. Guess again."
-        end
-        next
+        tries += 1   
+        handle_incorrect(question, tries)
       end
     end
   end
 
-  def start
-    puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
-    main_menu
+  def handle_incorrect(question, tries)
+    if tries > 2
+      puts "Incorrect. View answer? y/n"
+      input = gets.chomp
+      if input == "n"
+        return true
+      else
+        puts "The correct answer is: #{@deck.get_answer(question)}"
+        return false
+      end
+    else
+      puts "Incorrect. Guess again."
+      true
+    end
   end
 
 end
